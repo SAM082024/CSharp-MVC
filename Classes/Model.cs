093cdee879c8db118910ca552771381e3b541557
SAM082024 <sam082024@gmail.com>
2024-10-08 12:55:04 +0200

    added a new class and a videogame list along with its properties

--- Classes/Model.cs
+++ Classes/Model.cs
@@ -1,7 +1,24 @@
+using System.Reflection;
+
 public class VideoGame
 {
     public string? Title { get; set; }
     public int Year { get; set; }
     public string? Genre { get; set; }
     public string? Developer { get; set; }
+}
+
+public class Model
+{
+    public List<VideoGame> Games { get; set; }
+
+    public Model()
+    {
+        Games = new List<VideoGame>
+        {
+            new() { Title = "gameTitle", Year = 2000, Genre = "First-Person-Shooter", Developer = "gameDev"},
+            new() { Title = "gameTitle", Year = 2000, Genre = "First-Person-Shooter", Developer = "gameDev"},
+            new() { Title = "gameTitle", Year = 2000, Genre = "First-Person-Shooter", Developer = "gameDev"},
+        };
+    }
 }
