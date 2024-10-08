aadb7280a53b79c21b949c078cbbbf59479ad392
SAM082024 <sam082024@gmail.com>
2024-10-08 13:02:43 +0200

    created a class for the controller and references to both the model and view

---
+++ Classes/Controller.cs
@@ -0,0 +1,11 @@
+public class Controller
+{
+    private Model _model;
+    private View _view;
+
+    public Controller(Model model, View view)
+    {
+        _model = model;
+        _view = view;
+    }
+}
