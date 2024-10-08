919e4913c6169919535d29c0718f54f74155d2db
SAM082024 <sam082024@gmail.com>
2024-10-08 12:49:55 +0200

    added a VideoGame class with properties of: title, year, genre and developer

---
+++ Classes/Model.cs
@@ -0,0 +1,7 @@
+public class VideoGame
+{
+    public string? Title { get; set; }
+    public int Year { get; set; }
+    public string? Genre { get; set; }
+    public string? Developer { get; set; }
+}
