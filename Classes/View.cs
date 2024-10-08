371c900c4349cded414ba0520155a9f35bb67d47
SAM082024 <sam082024@gmail.com>
2024-10-08 12:58:55 +0200

    created a function to display video-games

--- Classes/View.cs
+++ Classes/View.cs
@@ -6,4 +6,17 @@ public class View
     {
         myModel = model;
     }
+
+    public void displayVideoGames()
+    {
+        Console.WriteLine("Video-Games in the model:\n");
+
+        foreach (var VideoGame in myModel.Games)
+        {
+            Console.WriteLine($"Title: {VideoGame.Title}\n");
+            Console.WriteLine($"Year: {VideoGame.Year}\n");
+            Console.WriteLine($"Genre: {VideoGame.Genre}\n");
+            Console.WriteLine($"Developers: {VideoGame.Developer}\n");
+        }
+    }
 }
